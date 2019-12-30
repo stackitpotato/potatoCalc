eeb633febc2030c4d5a4521361b615aef6252af2
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-30 12:58:05 +0000

    added js modal info

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ game = {
     score: 0
 }
 
-
-//top of the stack (count of items -1 to account for zero index)
-var top = game.potatoStack[game.potatoStack.count - 1]
-
 var stack = game.potatoStack
 
 var scores = game.potatoStackScores
@@ -53,6 +49,33 @@ document.querySelector('.mostChicken').addEventListener('click', function (e) {
 document.querySelector('.calculate').addEventListener('click', function (e) {
     e.preventDefault()
     calculateScore()
+})
+
+
+// Get the modal
+var modal = document.querySelector('.modal');
+
+// Get the button that opens the modal
+var btn = document.querySelector('.myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+btn.addEventListener('click', function () {
+    modal.style.display = "block";
+})
+
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener('click', function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function (e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
 })
 
 
